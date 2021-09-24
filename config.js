90e82a4f7020b5baa23c179784545dd1f6eda529
HKgaming012 <90050568+Hkgaming123@users.noreply.github.com>
2021-09-24 13:03:14 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.Prefix = `g!`;
+exports.Token = `ODg3MzU4NTMwODA4NTc4MDU4.YUC_CA.J92nB0SKcoq_OFvF6u3oDnQK8fA`;
+exports.Color = `RANDOM`;
--- index.js
+++ index.js
@@ -2,17 +2,62 @@ const express = require('express')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Yo boi!!'))
+app.get('/', (req, res) => res.send('make your bot 24/7 good innit!!'))
 
 app.listen(port, () =>
 console.log(`Your app is listening a http://localhost:${port}`)
 );
 const Discord = require('discord.js')
 const client = new Discord.Client
+const { Prefix, Token, Color } = require("./config.js");
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.db = require("quick.db");
 
 client.on("ready", () => {
-  console.log(`Bot is ready thanks to replit.it`)
-})
+  console.log(`${client.user.tag} is ready coded by Hkgaming btw`)
+  client.user
+      .setActivity(`Hkgaming`, { type: "LISTENING" })
+    .catch(error => console.log(error));
+});
+
+client.on("message", async message => {
+  if (message.channel.type === "dm") return;
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+
+    let modules = ["fun", "info", "moderation"];
+
+modules.forEach(function(module) {
+  fs.readdir(`./commands/${module}`, function(err, files) {
+    if (err)
+      return new Error(
+        "Missing Folder Of Commands! Example : Commands/<Folder>/<Command>.js"
+      );
+    files.forEach(function(file) {
+      if (!file.endsWith(".js")) return;
+      let command = require(`./commands/${module}/${file}`);
+      console.log(`${command.name} Command Has Been Loaded - ✅`);
+      if (command.name) client.commands.set(command.name, command);
+      if (command.aliases) {
+        command.aliases.forEach(alias =>
+          client.aliases.set(alias, command.name)
+        );
+      }
+      if (command.aliases.length === 0) command.aliases = null;
+    });
+  });
+});
+
+
+  if (message.content.match(new RegExp(`^<@!?${client.user.id}>`))) {
+    return message.channel.send(`Bot Prefix : ${Prefix}`);
+  }
+});
+
+//(simple messages)
 
 client.on("message", msg => {
   if (msg.content === "ping") {
@@ -20,18 +65,5 @@ client.on("message", msg => {
   }
 })
 
-if (command === 'embed') {
-  const embed = new Discord.MessageEmbed()
-  .setTitle("HELLO")
-  .setDescription("Description")
-  .setColor("GREEN")
-  .setImage("https://wallpaperaccess.com/full/526285.jpg")
-  .setAuthor("Author", "https://wallpaperaccess.com/full/526285.jpg")
-  .setFooter("footer")
-  .setTimeStamp()
-
-  message.channel.send(embed)
-  
-}
 
 client.login(process.env.TOKEN)
